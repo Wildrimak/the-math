a89d1c91d96f563651e7c2310799b515e5935f70
Wildrimak <finalwildrimak@gmail.com>
2020-01-11 12:47:38 -0300

    apenas backup

--- scripts/Tabuleiro.gd
+++ scripts/Tabuleiro.gd
@@ -1,26 +1,17 @@
 extends Node2D
 
 func _ready():
+	pass
+	#print(get_tree().get_current_scene().get_child_count())
+	#print(get_tree().get_current_scene().get_filename())
 	
-	print(get_tree().get_current_scene().get_child_count())
-	print(get_tree().get_current_scene().get_filename())
+	#var qtd_filhos = get_tree().get_current_scene().get_child_count()
 	
-	print(get_tree().get_nodes_in_group("Coluna8"))
-	
-	print(get_tree().get("Coluna8"))
-	
-	var qtd_filhos = get_tree().get_current_scene().get_child_count()
-	
-	for i in range(qtd_filhos):
-		#print(get_tree().get_current_scene().get_position_in_parent())
-		#print(i)
-		pass
-
 #func _process(delta):
 #	pass
 
 
 func _on_B8_click():
 	print("B8")
-	print(get_tree().get_current_scene())
-	print($".".position)
+	#print(get_tree().get_current_scene())
+	#print($".".position)
